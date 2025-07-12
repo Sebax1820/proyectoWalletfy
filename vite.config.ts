af5deed3bb6f0e71f675e83bd83ae5f82bb0ef48
Sebastian <sebax1820@gmail.com>
2025-07-12 11:12:54 -0500

    errores de configuracion

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigpaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    tsconfigpaths(),
+    tailwindcss(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact()],
+  
 })
